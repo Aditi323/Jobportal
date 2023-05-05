1accb2402f93ca60694afd209b47352c65087316
Aditi Bisoi <114893448+Aditi323@users.noreply.github.com>
2023-05-05 11:06:22 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,201 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="plugins/home-plugins/img/dev1.png" alt="showcase-section">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>JOB PORTAL</h3>
+						<p>This website serves jobseekers to find available job vacancies and Employers to identify eligible job seekers with the prospect of selecting the most qualified candidates. The only way to select best-qualified candidate is to have a pool of eligible applicants, which is possible by drawing the interest of individuals in the market. Job search portals best serve this purpose. E-recruitment has become the standard means for employers and job seekers to meet their respective objectives.</p>
+						 <p> This system can be used as an Online Job Portal for the Placements providing to the un employees who are seeking for a job placement. Job Seeker logging into the system and he can should be able to upload their information in the form of a CV. Visitors/Company representatives logging in may also access/search any information put up by Job Seeker.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>This website is for the job seekers to find their desired jobs and for the recruiters to find the right candidate. Job seekers from any background can search for the current job openings. Job seekers can register with the application and update their details and skill set.<br/><br/>They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 1
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										 They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance.
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 2
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance.
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 3
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Heading Text Item # 4
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseFour-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Aditi Bisoi</h4>
+										<span class="deg">Creative</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Presha Singh</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Bishnupriya pattnaik</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Niki</h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
---
+++ Contact.php
@@ -0,0 +1,57 @@
+ 
+	<section id="content">
+	
+	<div class="container">
+		<div class="row"> 
+							<div class="col-md-12">
+								<div class="about-logo">
+									<h3>THE JOB PORTAL</h3>
+									<p>This website serves jobseekers to find available job vacancies and Employers to identify eligible job seekers with the prospect of selecting the most qualified candidates. The only way to select best-qualified candidate is to have a pool of eligible applicants, which is possible by drawing the interest of individuals in the market. Job search portals best serve this purpose. E-recruitment has become the standard means for employers and job seekers to meet their respective objectives.</p>
+                                    	<p>This system can be used as an Online Job Portal for the Placements providing to the un employees who are seeking for a job placement. Job Seeker logging into the system and he can should be able to upload their information in the form of a CV. Visitors/Company representatives logging in may also access/search any information put up by Job Seeker.</p>
+								</div>  
+							</div>
+						</div>
+	<div class="row">
+								<div class="col-md-6">
+									<p>They are the cheapest as well as the fastest source of communication reaching wide range of audience on just a single click irrespective of their geographical distance.</p>
+								  	
+		   <!-- Form itself -->
+          <form name="sentMessage" id="contactForm"  novalidate>
+	       <h3>Contact me</h3>
+		 <div class="control-group">
+                    <div class="controls">
+			<input type="text" class="form-control" 
+			   	   placeholder="Full Name" id="name" required
+			           data-validation-required-message="Please enter your name" />
+			  <p class="help-block"></p>
+		   </div>
+	         </div> 	
+                <div class="control-group">
+                  <div class="controls">
+			<input type="email" class="form-control" placeholder="Email" 
+			   	            id="email" required
+			   		   data-validation-required-message="Please enter your email" />
+		</div>
+	    </div> 	
+			  
+               <div class="control-group">
+                 <div class="controls">
+				 <textarea rows="10" cols="100" class="form-control" 
+                       placeholder="Message" id="message" required
+		       data-validation-required-message="Please enter your message" minlength="5" 
+                       data-validation-minlength-message="Min 5 characters" 
+                        maxlength="999" style="resize:none"></textarea>
+		</div>
+               </div> 		 
+	     <div id="success"> </div> <!-- For success/fail messages -->
+	    <button type="submit" class="btn btn-primary pull-right">Send</button><br />
+          </form>
+								</div>
+								<div class="col-md-6">
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script><div style="overflow:hidden;height:500px;width:600px;"><div id="gmap_canvas" style="height:500px;width:600px;"></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style><a class="google-map-code" href="http://www.trivoo.net" id="get-map-data">trivoo</a></div><script type="text/javascript"> function init_map(){var myOptions = {zoom:14,center:new google.maps.LatLng(40.805478,-73.96522499999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(40.805478, -73.96522499999998)});infowindow = new google.maps.InfoWindow({content:"<b>The Breslin</b><br/>2880 Broadway<br/> New York" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+								</div>
+							</div>
+	</div>
+ 
+	</section>
+ 
